e765b0c75accc102188140785b7b5f034871bf63
stevepapa <dev@stevepapa.com>
2026-01-20 18:29:51 +1100

    refresh v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Autosize } from './autosize.directive';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [FormsModule, Autosize],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'app';
+  title = 'ng-autosize-demo';
+
+  basicText = '';
+  minHeightText = 'Try adding new lines…\nThis textarea has a min height.';
+  maxHeightText = 'This one has a max height.\nKeep typing to see it start scrolling.';
+
+  minHeight = 80;
+  maxHeight = 180;
+
+  setSample(autosize: Autosize): void {
+    this.basicText = `Programmatically set at ${new Date().toLocaleTimeString()}\n\nLine 3\nLine 4`;
+    autosize.refresh();
+  }
 }
